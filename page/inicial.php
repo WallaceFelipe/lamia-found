e221b738ba5c39adcc263fe1abf059bba846d663
João Oliveira <joao@zerobugs.com.br>
2016-10-23 21:21:17 -0200

    Adição do Layout

---
+++ page/inicial.php
@@ -0,0 +1,41 @@
+<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Nome do CRUD</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Nome do Crud</h1>
+			</div>
+		</div><!--/.row-->
+				
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Nome da tabela</div>
+					<div class="panel-body">
+						<table class='table table-boredered table-hover'>
+						    <thead>
+						    <tr>
+						        <th data-field="state" data-checkbox="true" >Item ID</th>
+						        <th data-field="id" data-sortable="true">Item ID</th>
+						        <th data-field="name"  data-sortable="true">Item Name</th>
+						        <th data-field="price" data-sortable="true">Item Price</th>
+						    </tr>
+						    </thead>
+						    <tbody>
+						    	<tr>
+						    		<td>#</td>
+						    		<td>#</td>
+						    		<td>#</td>
+						    		<td>#</td>
+						    	</tr>
+						    </tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div><!--/.row-->	
